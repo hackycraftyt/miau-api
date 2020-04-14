665e1a3d38a1fd0bcf77b9dcb2855cb794913aff
hackycraftyt <59286100+hackycraftyt@users.noreply.github.com>
2020-04-14 09:39:38 +0200

    Archivo principal

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 //Instala el npm: npm i -s miau-api
+const miau = require('miau-api')
+//Con el método async
+let img = await miau.gato()
 
-const cat = require('miau-api')
+console.log(img)
+
+//Para discord:
+//Necesitas obviamente el package de discord.js
+const miau require('miau-api')
+//En donde empiezas el evento message necesitas poner async: client.on('message', async message => {
+    //                                                                            ^^^^^
+const img = await miau.gato()
+
+//Mediante un mensaje normal:
+message.channel.send(img)
+//Mediante un embed:
+const embed = new Discord.MessageEmbed()
+.setImage(img)
+
+messge.channel.send(embed)
